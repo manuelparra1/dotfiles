fed1a146c61f0a5fae93e398f86879f96d731298
Manuel Parra <manuel.parra1@gmail.com>
2025-09-21 22:43:43 -0500

    nvim lspconfig reference update

--- .config/nvim/lua/dusts/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/dusts/plugins/lsp/lspconfig.lua
@@ -7,9 +7,6 @@ return {
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 	},
 	config = function()
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
-
 		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
@@ -57,43 +54,47 @@ return {
 			},
 		})
 
-		-- configure html server
-		lspconfig["html"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- NEW: Use vim.lsp.config instead of lspconfig
+    -- configure html server
+    vim.lsp.config.html.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		-- configure css server
-		lspconfig["cssls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- NEW: Use vim.lsp.config instead of lspconfig
+    -- configure css server
+    vim.lsp.config.cssls.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		-- configure python server
-		lspconfig["pyright"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- NEW: Use vim.lsp.config instead of lspconfig
+    -- configure python server
+    vim.lsp.config.pyright.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		-- configure lua server (with special settings)
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = { -- custom settings for lua
-				Lua = {
-					-- make the language server recognize "vim" global
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						-- make language server aware of runtime files
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
-						},
-					},
-				},
-			},
-		})
-	end,
+    -- NEW: Use vim.lsp.config instead of lspconfig
+    -- configure lua server (with special settings)
+    vim.lsp.config.lua_ls.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = { -- custom settings for lua
+        Lua = {
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            -- make language server aware of runtime files
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+          },
+        },
+      },
+    })
+  end,
 }
